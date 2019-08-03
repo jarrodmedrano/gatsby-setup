918ef2a8ccb04f19ad479e67ebd92230b6e492f8
Jarrod Medrano <jmedran@gmail.com>
2019-08-03 11:02:25 -0600

    adding google font

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,12 +6,21 @@
  */
 
 import React from "react"
-
-import Header from "./header"
 import MainMenu from "./MainMenu";
+
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css?family=Open+Sans|Roboto&display=swap');
+
+  body {
+    font-family: 'Open Sans', sans-serif;
+  }
+`;
 
 export default ({ children }) => (
   <div>
+    <GlobalStyles />
     <MainMenu></MainMenu>
     {children}
   </div>
