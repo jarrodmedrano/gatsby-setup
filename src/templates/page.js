35309dacfffc4d011fdb25e89154b6da3ac17481
Jarrod Medrano <jmedran@gmail.com>
2019-08-03 10:47:45 -0600

    add page content

--- src/templates/page.js
+++ src/templates/page.js
@@ -5,6 +5,7 @@ export default ({ pageContext }) => {
   return (
     <Layout>
       <h1 dangerouslySetInnerHTML={{ __html: pageContext.title }}></h1>
+      <div dangerouslySetInnerHTML={{ __html: pageContext.content }}></div>
     </Layout>
   )
 }
